12b76a2d101a559db0cbb2d0445380e8d5570277
/\ngel <25136754+serious-angel@users.noreply.github.com>
2025-09-12 14:50:21 +0200

    Set validation during a cast to be optional
    1. Added parameter for optional validation during a cast
    2. Since we except the data to be arrayable, let's change the type from BaseData to Data

--- src/SettingsCasts/ArrayDataCast.php
+++ src/SettingsCasts/ArrayDataCast.php
@@ -3,42 +3,53 @@
 namespace Spatie\LaravelSettings\SettingsCasts;
 
 use Exception;
-use Spatie\LaravelData\Contracts\BaseData;
+use Spatie\LaravelData\Data;
 use Spatie\LaravelSettings\SettingsCasts\SettingsCast;
 
 class ArrayDataCast implements SettingsCast
 {
     protected string $type;
 
-    public function __construct(?string $type)
+    protected bool $validate;
+
+    public function __construct(?string $type, string $validate = 'true')
     {
-        $this->type = $this->ensureDataTypeExists($type);
+        $this->type     = $this->ensureDataTypeExists($type);
+        $this->validate = !in_array($validate, ['false', '0'], true);
     }
 
     /**
      * @param array<array> $payload
      *
-     * @return BaseData[]
+     * @return Data[]
      */
     public function get($payload): array
     {
         return array_map(
-            fn ($data) => $this->type::validateAndCreate($data),
+            fn ($data) => $this->createData($data),
             $payload
         );
     }
 
     /**
-     * @param array<array|BaseData> $payload
+     * @param array<array|Data> $payload
      *
      * @return array<array>
      */
     public function set($payload): array
     {
         return array_map(
-            fn ($data) => $this->type::validateAndCreate($data)->toArray(),
+            fn ($data) => $this->createData($data)->toArray(),
             $payload
         );
+    }
+
+    /**
+     * @param array|Data $data
+     */
+    protected function createData($data): Data
+    {
+        return $this->validate ? $this->type::validateAndCreate($data) : $this->type::from($data);
     }
 
     protected function ensureDataTypeExists(?string $type): string
@@ -51,7 +62,7 @@ class ArrayDataCast implements SettingsCast
             throw new Exception("Cannot create a data cast for `$type` because the data does not exist");
         }
 
-        if (!class_implements($type, BaseData::class)) {
+        if (!class_implements($type, Data::class)) {
             throw new Exception("Cannot create a data cast for `$type` because the class does not implement data");
         }
 
